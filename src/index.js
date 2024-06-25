22d2277ff5bc09d71cecc55eb7fe2e9478f54235
Michael Hanel <mdens@gmx.net>
2024-06-25 14:48:32 +0200

    set

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ app.use(morgan("dev"))
 app.use(express.json())
 
 app.get("/", ( _, res) => {
-    res.send("..serving that Brathering Fish hot n' spicy.. :)")
+    res.send("..serving that Brathering hot n' spicy.. :)")
 })
 
 app.get("/api/products/all", async function getAllProductsController(_, res) {
